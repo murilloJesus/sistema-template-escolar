2f3cd0a1fc96d48f200c076a2aa5d261a08a94f8
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-10-05 17:41:09 -0300

    Adição de um template e finalização das cores por var

---
+++ resources/views/templates/miniport/components/base/header.blade.php
@@ -0,0 +1,9 @@
+	<!-- Nav -->
+    <nav id="nav">
+				<ul class="container">
+					<li><a href="#top">Home</a></li>
+					<li><a href="#work">Sobre Nós</a></li>
+					<li><a href="#portfolio">Galeria</a></li>
+					<li><a href="#contact">Entre em Contato</a></li>
+				</ul>
+			</nav>
