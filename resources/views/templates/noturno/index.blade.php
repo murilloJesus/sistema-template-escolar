f816eed41eab201a9411755fac60d42ce4d53e73
Leonardo Ortiz <leoortizinho23@gmail.com>
2022-10-05 16:04:26 -0300

    Template noturno

--- resources/views/templates/noturno/index.blade.php
+++ resources/views/templates/noturno/index.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Noturno</title>
+	<link rel="stylesheet" href="/public/storage/css/css.css" />
     <link href="https://fonts.googleapis.com/css?family=Heebo:400,700|IBM+Plex+Sans:600" rel="stylesheet">
     <link rel="stylesheet" href="/public/assets/noturno/css/style.css">
     <script src="https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js"></script>
